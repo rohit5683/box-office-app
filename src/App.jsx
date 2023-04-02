0dbe678c98f0d6c28293d39da925e1714c419bb3
rohit5683 <killerrohitrocks007@gmail.com>
2023-04-03 00:32:25 +0530

    Added React-Router-Dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+import Contact from './Pages/Contact';
 
 function App() {
-  return <div>Hello</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<div> Page Not Found</div>} />
+        {/* <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="teams" element={<Teams />}>
+          <Route path=":teamId" element={<Team />} />
+          <Route path="new" element={<NewTeamForm />} />
+          <Route index element={<LeagueStandings />} />
+        </Route>
+      </Route>
+      <Route element={<PageLayout />}>
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/tos" element={<Tos />} />
+      </Route>
+      <Route path="contact-us" element={<Contact />} /> */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
