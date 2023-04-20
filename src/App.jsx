f488451bb7cb17edf31f7ef2978ff3033924337e
rohit5683 <killerrohitrocks007@gmail.com>
2023-04-21 02:24:37 +0530

    installed react-query. replace useShowById with useQuery Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
 import Contact from './Pages/Contact';
 import MainLayout from './Components/MainLayout';
 import Show from './Pages/Show';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-        </Route>
-        <Route path="*" element={<div> Page Not Found</div>} />
-        <Route path="/show/:showId" element={<Show />} />
-      </Routes>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+          </Route>
+          <Route path="*" element={<div> Page Not Found</div>} />
+          <Route path="/show/:showId" element={<Show />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
--- src/Pages/Show.jsx
+++ src/Pages/Show.jsx
@@ -1,33 +1,13 @@
-import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getShowById } from './api/tvmaze';
-import { useState } from 'react';
-
-const useShowById = showId => {
-  const [showData, setShowData] = useState(null);
-  const [showError, setShowError] = useState(null);
-
-  useEffect(() => {
-    // getShowById().
-
-    async function fetchData() {
-      try {
-        const data = await getShowById(showId);
-        setShowData(data);
-      } catch (err) {
-        setShowError(err);
-      }
-    }
-
-    fetchData();
-  }, [showId]);
-
-  return { showData, showError };
-};
+import { QueryClientProvider, useQuery } from '@tanstack/react-query';
 
 const Show = () => {
   const { showId } = useParams();
-  const { showData, showError } = useShowById(showId);
+  const { data: showData, error: showError } = useQuery({
+    queryKey: ['show', showId],
+    queryFn: () => getShowById(showId),
+  });
 
   if (showError) {
     return <div>We have an Error: {showError.message}</div>;
