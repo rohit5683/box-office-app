b4b249ef1e80e49cfad5ebd99515952724a730ec
rohit5683 <killerrohitrocks007@gmail.com>
2023-04-26 01:23:00 +0530

    Fetch starred shows data and display ShowGird for them

--- src/Pages/Starred.jsx
+++ src/Pages/Starred.jsx
@@ -1,8 +1,33 @@
+import { useQuery } from '@tanstack/react-query';
 import { useStarredShows } from '../Lib/useStarredShows';
+import { getShowByIds } from './api/tvmaze';
+import ShowGrid from '../Components/Shows/ShowGrid';
 
 const Starred = () => {
-  const [starredShows] = useStarredShows();
-  return <div>Starred Page, Starred {starredShows.length}</div>;
+  const [starredShowsIds] = useStarredShows();
+
+  const { data: starredShows, error: starredShowsError } = useQuery({
+    queryKey: ['starred', starredShowsIds],
+    queryFn: () =>
+      getShowByIds(starredShowsIds).then(result =>
+        result.map(show => ({ show }))
+      ),
+    refetchOnWindowFocus: false,
+  });
+
+  if (starredShows?.length === 0) {
+    return <div> No Shows are Starred </div>;
+  }
+
+  if (starredShows?.length > 0) {
+    return <ShowGrid shows={starredShows} />;
+  }
+
+  if (starredShowsError) {
+    return <div>Error Occured: {starredShowsError}</div>;
+  }
+
+  return <div>Shows are Loading</div>;
 };
 
 export default Starred;
